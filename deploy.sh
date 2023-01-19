b21382a73d8f5baca52b93a792c0c03fcf36e051
philipp.schoeppner <philipp.schoeppner@fntsoftware.com>
2023-01-19 21:53:10 +0100

    feat(logging): Added Grafana Loki and Promtail

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,12 @@
 
 set -e
 
+# vars
+K8S_CONTEXT=kind-coding-challenge-cluster
+MONITORING_NAMESPACE=monitoring
+LOGGING_NAMESPACE=logging
+APP_NAMESPACE=logging
+
 # create cluster
 if [[ $(kind get clusters | grep coding-challenge-cluster) == "" ]]; then
     kind create cluster --config=./kind/config.yaml
@@ -9,8 +15,8 @@ fi
 
 # deploy monitoring stack
 helm upgrade \
-    --kube-context kind-coding-challenge-cluster \
-    --namespace monitoring \
+    --kube-context $K8S_CONTEXT \
+    --namespace $MONITORING_NAMESPACE \
     --create-namespace \
     --install \
     monitoring \
@@ -19,3 +25,19 @@ helm upgrade \
 
 # deploy demo app
 kubectl apply -k ./k8s/kustomize/kuard
+
+# challenge 1: Install logging (Grafana Loki/Promtail)
+helm upgrade \
+    --install \
+    --create-namespace \
+    --namespace $LOGGING_NAMESPACE \
+    --kube-context $K8S_CONTEXT \
+    --values ./k8s/helm/loki/values.yaml \
+    loki grafana/loki
+helm upgrade \
+    --install \
+    --create-namespace \
+    --namespace $LOGGING_NAMESPACE \
+    --kube-context $K8S_CONTEXT \
+    --values ./k8s/helm/promtail/values.yaml\
+    promtail grafana/promtail
